343309f891e03b87097cb36f0526fc6bf2b0cd17
Luke Vidler <luke.vidler@gmail.com>
2025-10-07 18:58:11 +1100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,7 +22,8 @@ func main() {
 			log.Fatal(err)
 		}
 
-		header := make([]byte, 100)
+		// Read the first 108 bytes to include file header and B-tree page header
+		header := make([]byte, 108)
 
 		_, err = databaseFile.Read(header)
 		if err != nil {
@@ -34,11 +35,22 @@ func main() {
 			fmt.Println("Failed to read integer:", err)
 			return
 		}
+
+		// Read number of cells from the B-tree page header
+		// The page header starts at offset 100 (after the 100-byte file header)
+		// Cell count is at offset 3-4 of the page header (offset 103-104 in the file)
+		var cellCount uint16
+		if err := binary.Read(bytes.NewReader(header[103:105]), binary.BigEndian, &cellCount); err != nil {
+			fmt.Println("Failed to read cell count:", err)
+			return
+		}
+
 		// You can use print statements as follows for debugging, they'll be visible when running tests.
 		fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 		// Uncomment this to pass the first stage
-		fmt.Printf("database page size: %v", pageSize)
+		fmt.Printf("database page size: %v\n", pageSize)
+		fmt.Printf("number of tables: %v\n", cellCount)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
